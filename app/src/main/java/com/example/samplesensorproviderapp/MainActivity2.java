e9c539211e2ce9940eda8224aec2fdeca4decbd3
adrielmori <adrielmori@outlook.com>
2024-01-09 16:30:42 -0300

    Chore: Conflits remove

--- app/src/main/java/com/example/samplesensorproviderapp/MainActivity2.java
+++ app/src/main/java/com/example/samplesensorproviderapp/MainActivity2.java
@@ -1,26 +1,23 @@
 package com.example.samplesensorproviderapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.webkit.WebView;
 import android.widget.TextView;
 
-public class MainActivity2 extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private SensorManager sensor;
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        sensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SensorManager sensor = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         TextView textView = (TextView) findViewById(R.id.textView2);
 
-        AccelerometerSensorAccess accAcess = new AccelerometerSensorAccess(sensor, textView);
+        new AccelerometerSensorAccess(sensor, textView);
     }
 }
